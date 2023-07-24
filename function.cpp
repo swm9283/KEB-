8ed20bf380388d73fbf28bc89251393c6f0f8dd8
uminsong1512 <uminsong1512@gmail.com>
2023-07-24 23:57:35 +0900

    function 02 인수 전달 방법 - 값에 의한 전달.(call by value)

--- function.cpp
+++ function.cpp
@@ -1,31 +1,17 @@
-//function.
+// function 02 인수 전달 방법 - 값에 의한 전달.(call by value).
 # include <iostream>
 using namespace std;
 
-int SmallNum(int, int);
-//main 함수 아래에 함수를 정의할 경우. compiler가 가장 먼저 main 함수를 compile하기 때문에
-// SmallNum함수를 인식못할 수 있다. 따라서 이를 방지하고자 "한수의 원형 선언을 한다."
+void Local(int); // 함수 원형 선언.
 
 int main(void)
 {
-    int input1;
-    int input2;
-    cout << "숫자1을 입력해주세요 : " << endl;
-    cin >> input1;
-    cout << "숫자2를 입력해주세요 : " << endl;
-    cin >> input2;
-    cout << SmallNum(input1, input2) << endl;
+    int var = 10;
+    cout << "변수 var의 초깃값은 " << var << "입니다." << endl;
+
+    Local(var);
+    cout << "Local() 함수 호출 후 변수 var 값은 " << var << "입니다." << endl;
     return 0;
 }
 
-int SmallNum(int num1, int num2)
-{
-    if (num1 < num2)
-    {
-        return num1;
-    }
-    else
-    {
-        return num2;
-    }
-};
+void Local(int num) { num += 10; }
