a7460c547b6aa038e32a16a70f6fa1d5096d3281
uminsong1512 <uminsong1512@gmail.com>
2023-07-24 18:13:44 +0900

    structure_type 03 union 공용체

--- structure_type.cpp
+++ structure_type.cpp
@@ -1,30 +1,23 @@
+// 공용체와 열거체
 # include <iostream>
 # include <string>
 using namespace std;
 
-struct Name
+union ShareData
 {
-    string first;
-    string last;
-};
-
-
-struct Friends
-{
-    Name first_name; // 중첩된 구조체.
-    string address;
-    string job;
+    unsigned char a;
+    unsigned short b;
+    unsigned int c;
 };
 
 int main(void)
 {
-    Friends hong = {
-        {"우민", "송"}, "인천광역시 미추홀구", "Bioinfomatics researcher"};
+    ShareData var;
+    var.c = 0x12345678;
 
-    cout << hong.address << endl << endl;
-    cout << hong.first_name.last << hong.first_name.first << "에게" << endl;
-    cout << "그동안 잘 지냈니? 아직도" << hong.job << "이니" << endl;
-    cout << "다음에 꼭 한 번 만나자." << endl << "잘 지내" <<endl ;
-    cout << sizeof(Friends); // 구조체의 크기 : 멤버변수에 영향을 받음.
+    cout << hex;
+    cout << var.a << endl;
+    cout << var.b << endl;
+    cout << var.c;
     return 0;
 }
