8088e9bd280761fe191f5dfea8b3e048e34cda53
uminsong1512 <uminsong1512@gmail.com>
2023-07-24 17:45:43 +0900

    structure_type 02

--- structure_type.cpp
+++ structure_type.cpp
@@ -2,39 +2,29 @@
 # include <string>
 using namespace std;
 
-// 구조체를 가리키는 포인터를 인수로 전달하는 방식
-// 구조체의 복사본이 아닌 주소 하나만을 전달하므로 빠르다는 장점.
-// 원본 구조체에 직접 접근하므로, 원본 데이터의 보호 측면에서는 매우 위험.
-//const를 통해 원본 데이터 수정을 막음.
-struct prop
+struct Name
 {
-    int save;
-    int loan;
+    string first;
+    string last;
 };
 
-prop InitProperty(void);
-int CalcProperty(const prop *);
 
-
-int main(void) 
+struct Friends
 {
-    prop hong;
-    int hong_prop;
+    Name first_name; // 중첩된 구조체.
+    string address;
+    string job;
+};
 
-    hong = InitProperty();
-    hong_prop = CalcProperty(&hong);
-
-    cout << hong_prop << endl;
-}
-
-prop InitProperty(void)
+int main(void)
 {
-    prop hong_prop = {1000, 2000};
-    return hong_prop;
-}
+    Friends hong = {
+        {"우민", "송"}, "인천광역시 미추홀구", "Bioinfomatics researcher"};
 
-int CalcProperty(const prop* money)
-{
-    // money->save = 100;
-    return (money->save - money->loan);
+    cout << hong.address << endl << endl;
+    cout << hong.first_name.last << hong.first_name.first << "에게" << endl;
+    cout << "그동안 잘 지냈니? 아직도" << hong.job << "이니" << endl;
+    cout << "다음에 꼭 한 번 만나자." << endl << "잘 지내" <<endl ;
+    cout << sizeof(Friends); // 구조체의 크기 : 멤버변수에 영향을 받음.
+    return 0;
 }
