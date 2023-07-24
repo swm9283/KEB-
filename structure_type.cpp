898388485ab1eaca1f3667178652e9fefa27d8bd
uminsong1512 <uminsong1512@gmail.com>
2023-07-24 18:23:37 +0900

    structure_type 04 enmerated types

--- structure_type.cpp
+++ structure_type.cpp
@@ -3,21 +3,38 @@
 # include <string>
 using namespace std;
 
-union ShareData
-{
-    unsigned char a;
-    unsigned short b;
-    unsigned int c;
-};
+enum Weather { SUNNY = 0, CLOUD = 10, RAIN = 20, SNOW = 30};
 
 int main(void)
 {
-    ShareData var;
-    var.c = 0x12345678;
+    int input;
+    Weather weather;
 
-    cout << hex;
-    cout << var.a << endl;
-    cout << var.b << endl;
-    cout << var.c;
-    return 0;
+    cout << "오늘의 날씨를 입력해주세요 : " << endl;
+    cout << "(SUNNY=0, CLOUD=10, RAIN=20, SNOW=30)" << endl;
+    cin >> input;
+    weather = (Weather)input;
+    
+    switch(weather)
+    {
+        case SUNNY:
+        cout << "오늘의 날씨는 아주 맑아요";
+        break;
+
+        case CLOUD:
+        cout << "오늘의 날씨는 흐리네요";
+        break;
+        
+        case RAIN:
+        cout << "오늘의 날씨는 비가 주룩주룩 오네요!";
+        break;
+        case SNOW:
+        cout << "오늘의 날씨는 하얀 눈이 내려요";
+        break;
+
+        default:
+        cout << "제대로 입력해라 쉐이야";
+        break;
+     }
+     return 0;
 }
