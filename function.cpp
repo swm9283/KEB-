2faf3ce74b7e0247daf5a10780856dc1c67e4435
uminsong1512 <uminsong1512@gmail.com>
2023-07-25 13:16:59 +0900

    function 04 구조체의 참조

--- function.cpp
+++ function.cpp
@@ -1,33 +1,28 @@
-// function 04 참조자.
-// C++에서 함수의 인수로 참조자를 사용하는 방법의 특징
-//1. 함수 내에서 참조 연산자(*)를 사용하지 않으므로, 함수 내부의 코드가 깔끔하고 직관적
-//2. 함수의 호출이 값에 의한 전달(call by value)방법과 같은 형태가 되어, 코드를 읽기가 쉽지 않다.
-// 결론:
-// C++에서 크기가 큰 구조체나 클래스를 다룰 때만에만 참조자를 사용하고,
-// 간단한 함수에서는 call by reference보다는 call by value가 가독성이 좋을 수도 있다.
-
-
+// function 04 구조체의 참조.
 # include <iostream>
+# include <string>
 using namespace std;
-void Swap(int &, int &);
+
+struct Book
+{
+    string title;
+    string author;
+    int price;
+};
+
+void Display(const Book &); // 함수 원형 선언. call by reference
 
 int main(void)
 {
-    int x = 10; // 변수의 선언
-    int &y = x; // 참조자의 선언.
-
-    int num1 = 3, num2 = 7;
-    cout << "변경 전 num1의 값은 " << num1 << "이며, num2의 값은 " << num2 << "입니다." << endl;
-    Swap(num1, num2);
-	cout << "변경 후 num1의 값은 " << num1 << "이며, num2의 값은 " << num2 << "입니다." << endl;
-	return 0;
-
+    Book web_book = {"HTML과 CSS","홍길동",28000};
+    Display(web_book);
+    return 0;
 }
 
-void Swap(int& x, int& y)
+// const 키워드를 사용하여 원본 구조체에 대한 변경을 허용하지 않는 것이 좋다.
+void Display(const Book& bk) 
 {
-    int temp;
-    temp = x;
-    x = y;
-    y = temp;
+    cout << "책 제목 : " <<bk.title << endl;
+    cout << "저자명  : " << bk.author << endl;
+    cout << "책 가격 : " << bk.price << endl;
 }
