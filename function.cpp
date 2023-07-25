06feffe00095092503bdfe0616fad8d556aebec9
uminsong1512 <uminsong1512@gmail.com>
2023-07-25 13:10:48 +0900

    fuction 04 참조자

--- function.cpp
+++ function.cpp
@@ -1,57 +1,33 @@
-// function 03 함수 포인터
+// function 04 참조자.
+// C++에서 함수의 인수로 참조자를 사용하는 방법의 특징
+//1. 함수 내에서 참조 연산자(*)를 사용하지 않으므로, 함수 내부의 코드가 깔끔하고 직관적
+//2. 함수의 호출이 값에 의한 전달(call by value)방법과 같은 형태가 되어, 코드를 읽기가 쉽지 않다.
+// 결론:
+// C++에서 크기가 큰 구조체나 클래스를 다룰 때만에만 참조자를 사용하고,
+// 간단한 함수에서는 call by reference보다는 call by value가 가독성이 좋을 수도 있다.
+
+
 # include <iostream>
 using namespace std;
-
-typedef double (*Arith)(double, double); // typedef 키워드를 이용한 새로운 이름 선언!
-
-double Add(double, double);
-double Sub(double, double);
-double Mul(double, double);
-double Div(double, double);
-double Calculator(double, double, double (*func)(double, double));
-
+void Swap(int &, int &);
 
 int main(void)
 {
-    // double (*calc)(double, double) = NULL; // 함수 포인터 선언.
-    Arith calc = NULL; // 함수 포인터의 선언
-    double num1 = 3, num2 = 4, result = 0;
-    char oper;
-    cout << "원하는 계산을 입력하세요!"<<endl;
-    cout << "Addition : + Subtraction : - Multiplication : * Divide : / "
-         << endl;
-    cin >> oper;
+    int x = 10; // 변수의 선언
+    int &y = x; // 참조자의 선언.
 
-    switch (oper)
-    {
-        case '+' :
-            calc = Add;
-            break;
-        case '-':
-            calc = Sub;
-            break;
-        case '*':
-            calc = Mul;
-            break;
-        case '/':
-            calc = Div;
-            break;
-        default:
-        	cout << "사칙연산(+, -, *, /)만을 지원합니다.";
-            break;
-    }
+    int num1 = 3, num2 = 7;
+    cout << "변경 전 num1의 값은 " << num1 << "이며, num2의 값은 " << num2 << "입니다." << endl;
+    Swap(num1, num2);
+	cout << "변경 후 num1의 값은 " << num1 << "이며, num2의 값은 " << num2 << "입니다." << endl;
+	return 0;
 
-    result = Calculator(num1, num2, calc);
-    cout << "계산 결과는 : " << result << "입니다.." << endl;
-
-    return 0;
 }
 
-double Add(double num1, double num2) { return num1 + num2;}
-double Sub(double num1, double num2) { return num1 - num2; }
-double Mul(double num1, double num2) { return num1 * num2; }
-double Div(double num1, double num2) { return num1 / num2; }
-double Calculator(double num1, double num2, double(*func)(double, double))
+void Swap(int& x, int& y)
 {
-    return func(num1, num2);
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
 }
