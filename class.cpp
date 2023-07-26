869f02c7e5229f5b8d11a05699071096328a2f95
uminsong1512 <uminsong1512@gmail.com>
2023-07-26 22:00:40 +0900

    class 08 OOP 다형성 - 가상함수

--- class.cpp
+++ class.cpp
@@ -1,56 +1,28 @@
-// class 07 OOP 상속성 - 파생 클래스.
-#include <iostream>
-#include <string>
+// class 08 OOP 다형성 - 가상함수
+# include <iostream>
 using namespace std;
 
-class Person
+class A
 {
-private:
-	string name_;
-	int age_;
-public:
-	Person(const string& name, int age);	// 기초 클래스 생성자의 선언 
-	void ShowPersonInfo();
+    public:
+      virtual void Print() { cout << "A class의 Print 함수 입니다." << endl; };
 };
 
-class Student : public Person
+class B : public A
 {
-private:
-	int student_id_;
-public:
-	Student(int sid, const string& name, int age);	// 파생 클래스 생성자의 선언
-        void ShowPersonInfo(); // 파생 클래스에서 상속받은 멤버 함수의 재정의.(override)
+    public :
+      virtual void Print() { cout << "B class의 Print 함수 입니다." << endl; };
 };
 
 int main(void)
 {
-        Student song(12180000, "우민", 24);
-        song.ShowPersonInfo();
-        Student jang(1222, "서진", 21);
-        jang.ShowPersonInfo();
-        jang.Person::ShowPersonInfo(); // 범위 지정 연산자를 통해 기초클래스 원래 멤버함수 호출 가능.
+    A* ptr;
+    A obj_A;
+    B obj_B;
+    ptr = &obj_A;
+    ptr->Print();
 
-        return 0;
-}
-
-Person::Person(const string& name, int age)	// 기초 클래스 생성자의 정의 
-{
-	name_ = name;
-	age_ = age;
-}
-
-void Person::ShowPersonInfo()
-{
-	cout << this->name_ << "의 나이는 " << this->age_ << "살입니다." << endl;
-}
-
-Student::Student(int sid, const string& name, int age) : Person(name, age)	// 파생 클래스 생성자의 정의 
-{
-	student_id_ = sid;
-}
-
-void Student::ShowPersonInfo()
-{
-        cout << "이 학생은 인하대학교 학생이며 학번은 " << this->student_id_
-             << " 입니다." << endl;
+    ptr = &obj_B;
+    ptr->Print();
+    return 0;
 }
